3e2178f1348f0bd93b3b2722c9e7f85736d5973c
Your Name <AlvaroDG00>
2025-03-18 00:13:39 +0100

    verision 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 import java.lang.StringBuilder;
 class Solution{
 
+    /**
+     *
+     * @param s
+     * @return
+     */
     static String toCamelCase(String s){
-        return "";
+        String letras[] = s.split("");
+        String texto = "";
+
+        for (int i = 0; i < letras.length; i++){
+            if (letras[i].equals("-") || letras[i].equals("-")) {
+                texto = texto + letras[i+1].toUpperCase();
+                i++;
+            }else{
+                texto = texto + letras[i];
+            }
+        }
+        return texto;
     }
 }
